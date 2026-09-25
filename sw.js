21d9871545c8d77c8e4d2c19a1fc070db7198b44
anlatbibakalim <anlatbi.bakalim@gmail.com>
2026-09-25 23:13:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-var CACHE="bina-panel-v2";
+var CACHE="bina-panel-v3";
 var ASSETS=["./","./index.html","./manifest.json","./bina-ikon-192.png","./bina-ikon-512.png"];
-self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));self.skipWaiting();});
-self.addEventListener("activate",function(e){e.waitUntil(self.clients.claim());});
+self.addEventListener("install",function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){self.skipWaiting();}));
+});
+self.addEventListener("activate",function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){
+      if(k.indexOf("bina-panel")===0&&k!==CACHE){return caches.delete(k);}
+    }));
+  }).then(function(){return self.clients.claim();}));
+});
 self.addEventListener("fetch",function(e){
   if(e.request.method!=="GET")return;
   e.respondWith(caches.match(e.request).then(function(r){
